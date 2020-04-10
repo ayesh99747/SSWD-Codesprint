1ec764a12bbce82735a9a34cfad7061e162297d7
sumuduFernando <51757886+sumuduFdo@users.noreply.github.com>
2020-04-10 11:07:12 +0530

    uploading rating form

---
+++ rating_form.php
@@ -0,0 +1,37 @@
+<?php
+include ("db.php");
+include ("detectlogin.php");
+//create and populate pagename variable
+$pagename = "Submit Review";
+
+//use the stylesheet
+echo "<link rel=stylesheet type=text/css href=mystylesheet.css>";
+
+echo "<title>".$pagename."</title>";
+echo "<body>";
+
+include ("headfile.html");
+
+echo "<h4>".$pagename."</h4>";
+
+echo "<form id=reviewForm method=post action=>";
+	echo "<table style='border:0px'>";
+		echo "<tr>";
+			echo "<td style='border:0px'><label>Rating in short: </label></td>";
+			echo "<td style='border:0px'><input type=text name=ratingshort></td>";
+		echo "</tr>";
+		echo "<tr>";
+			echo "<td style='border:0px'><label>Your Review: </label></td>";
+			echo "<td style='border:0px'><textarea form=reviewForm name=ratinglong rows=5 cols=80></textarea></td>";
+		echo "</tr>";
+		echo "<tr>";
+			echo "<td style='border:0px'><input type=reset value='Clear'></td>";
+			echo "<td style='border:0px'><input type=submit value='Submit Review'></td>";
+		echo "</tr>";
+	echo "</table>";
+echo "</form>";
+
+include ("footfile.html");
+echo "</body>";
+
+?>
