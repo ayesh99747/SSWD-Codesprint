a0d33772954cecd889a05827ffce3d36b9913c5e
SafiyyahR <safiyyahrahman00@gmail.com>
2020-04-10 12:16:07 +0530

    Done changes to products.php

--- products.php
+++ products.php
@@ -32,16 +32,16 @@ if (!isempty($searchValue) || !isempty($searchCategory) || !isempty($sortOrder))
         while ($arrayp = mysqli_fetch_array($exeSQL)) {
             echo "<tr>";
             echo "<td style='border: 0px'>";
-            echo "<a href=prodbuy.php?u_prod_id=" . $arrayp['prodId'] . "&view=yes>";
+            echo "<a href=prodbuy.php?u_prod_id=" . $arrayp['testId'] . "&view=yes>";
             //display the small image whose name is contained in the array
-            echo "<img src=images/" .$arrayp['prodPicNameSmall']. " height=250 width=250>";
+            echo "<img src=images/" .$arrayp['testPicNameSmall']. " height=250 width=250>";
             echo "</a>";
             echo "</td>";
             echo "<td style='border: 0px'>";
-            echo "<p><h5>" . $arrayp['prodName'] . "</h5>"; //display product name as contained in the array
-            echo "<p>" . $arrayp['prodDescripShort'];
-            echo "<p><b>&euro;" . $arrayp['prodPrice'] . "</b>";
-            echo "<p><a href=ratings.php?u_prod_id=" . $arrayp['prodId'] . ">Test Ratings</a></p>";
+            echo "<p><h5>" . $arrayp['testName'] . "</h5>"; //display product name as contained in the array
+            echo "<p>" . $arrayp['testDescripShort'];
+            echo "<p><b>&euro;" . $arrayp['testPrice'] . "</b>";
+            echo "<p><a href=ratings.php?u_prod_id=" . $arrayp['testId'] . ">Test Ratings</a></p>";
             echo "</td>";
             echo "</tr>";
         }
@@ -58,16 +58,16 @@ if (!isempty($searchValue) || !isempty($searchCategory) || !isempty($sortOrder))
         while ($arrayp = mysqli_fetch_array($exeSQL)) {
             echo "<tr>";
             echo "<td style='border: 0px'>";
-            echo "<a href=prodbuy.php?u_prod_id=" . $arrayp['prodId'] . "&view=yes>";
+            echo "<a href=prodbuy.php?u_prod_id=" . $arrayp['testId'] . "&view=yes>";
             //display the small image whose name is contained in the array
-            echo "<img src=images/" .$arrayp['prodPicNameSmall']. " height=250 width=250>";
+            echo "<img src=images/" .$arrayp['testPicNameSmall']. " height=250 width=250>";
             echo "</a>";
             echo "</td>";
             echo "<td style='border: 0px'>";
-            echo "<p><h5>" . $arrayp['prodName'] . "</h5>"; //display product name as contained in the array
-            echo "<p>" . $arrayp['prodDescripShort'];
-            echo "<p><b>&euro;" . $arrayp['prodPrice'] . "</b>";
-            echo "<p><a href=ratings.php?u_prod_id=" . $arrayp['prodId'] . ">Test Ratings</a></p>";
+            echo "<p><h5>" . $arrayp['testName'] . "</h5>"; //display product name as contained in the array
+            echo "<p>" . $arrayp['testDescripShort'];
+            echo "<p><b>&euro;" . $arrayp['testPrice'] . "</b>";
+            echo "<p><a href=ratings.php?u_prod_id=" . $arrayp['testId'] . ">Test Ratings</a></p>";
             echo "</td>";
             echo "</tr>";
         }
