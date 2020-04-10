ef23738da9c175133421bbc26218c85697b3e0da
Ayeshmantha W <ayesh99747@gmail.com>
2020-04-10 11:20:58 +0530

    Completed index

--- index.php
+++ index.php
@@ -14,19 +14,52 @@ include("detectlogin.php");
 echo "<h4>" . $pagename . "</h4>"; //display name of the page on the web page
 
 echo "<form action='/products.php' method='GET'>";
+echo "<table>";
+    echo "<tr>";
+    echo "<td style='border: 0px'>";
     echo "<label for='searchValue'>Search - </label>";
+    echo "</td>";
+    echo "<td style='border: 0px'>";
     echo "<input type='text' placeholder='Search' name='searchValue'>";
+    echo "</td>";
+    echo "</tr>";
 
+    echo "<tr>";
+    echo "<td style='border: 0px'>";
     echo "<label for='options'>Search By - </label>";
+    echo "</td>";
+    echo "<td style='border: 0px'>";
     echo "<select id='options'>";
+    echo "<option value=''>Select Search Category</option>";
     echo "<option value='availability'>availability</option>";
     echo "<option value='cost'>cost</option>";
     echo "</select>";
-    
-    echo "<input type='submit' value='Submit'>";
+    echo "</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td style='border: 0px'>";
+    echo "<label for='options'>Sort By - </label>";
+    echo "</td>";
+    echo "<td style='border: 0px'>";
+    echo "<select id='options'>";
+    echo "<option value=''>Select Sorting</option>";
+    echo "<option value='ascending'>ascending</option>";
+    echo "<option value='descending'>descending</option>";
+    echo "</select>";
+    echo "</td>";
+    echo "</tr>";
+
+    echo "<tr>";
+    echo "<td style='border: 0px'>";
+    echo "<input type='submit' value='Search'>";
+    echo "</td>";
+    echo "<td style='border: 0px'>";
+    echo "</td>";
+    echo "</tr>";
+echo "</table>";
 echo "</form>";
-    
-          
+
 include("footfile.html"); //include foot layout
 
 echo "</body>";
