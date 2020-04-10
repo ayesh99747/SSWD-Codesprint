baf7621d884af0b9f88dd807f1cca6aca2cad653
Ayeshmantha W <ayesh99747@gmail.com>
2020-04-10 12:00:30 +0530

    added search

--- products.php
+++ products.php
@@ -15,37 +15,64 @@ echo "<h4>" . $pagename . "</h4>"; //display name of the page on the web page
 
 
 $searchValue = $_GET['searchValue'];
-$searchCategory = $_GET['searchValue'];
-$sortOrder = $_GET['searchValue'];
+$searchCategory = $_GET['searchBy'];
+$sortOrder = $_GET['orderBy'];
+$searchType = $_GET['searchType'];
 
 if (!isempty($searchValue) || !isempty($searchCategory) || !isempty($sortOrder)) {
-    # code...
+    if (isempty($searchCategory)) {
+        //create a $SQL variable and populate it with a SQL statement that retrieves product details
+        $SQL = "SELECT `testId`, `testName`, `testPicNameSmall`, `testPicNameLarge`, `testDescripShort`, `testDescripLong`, `testPrice` FROM `tests` WHERE testName LIKE '%test%'";
+        //run SQL query for connected DB or exit and display error message
+        $exeSQL = mysqli_query($conn, $SQL) or die(mysqli_error($conn));
+        echo "<table style='border: 0px'>";
+        //create an array of records (2 dimensional variable) called $arrayp.
+        //populate it with the records retrieved by the SQL query previously executed.
+        //Iterate through the array i.e while the end of the array has not been reached, run through it
+        while ($arrayp = mysqli_fetch_array($exeSQL)) {
+            echo "<tr>";
+            echo "<td style='border: 0px'>";
+            echo "<a href=prodbuy.php?u_prod_id=" . $arrayp['prodId'] . "&view=yes>";
+            //display the small image whose name is contained in the array
+            echo "<img src=images/" .$arrayp['prodPicNameSmall']. " height=250 width=250>";
+            echo "</a>";
+            echo "</td>";
+            echo "<td style='border: 0px'>";
+            echo "<p><h5>" . $arrayp['prodName'] . "</h5>"; //display product name as contained in the array
+            echo "<p>" . $arrayp['prodDescripShort'];
+            echo "<p><b>&euro;" . $arrayp['prodPrice'] . "</b>";
+            echo "<p><a href=ratings.php?u_prod_id=" . $arrayp['prodId'] . ">Test Ratings</a></p>";
+            echo "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
 } else {
     //create a $SQL variable and populate it with a SQL statement that retrieves product details
-$SQL = "select prodId, prodName, prodPicNameSmall, prodDescripShort, prodPrice from Product";
-//run SQL query for connected DB or exit and display error message
-$exeSQL = mysqli_query($conn, $SQL) or die(mysqli_error($conn));
-echo "<table style='border: 0px'>";
-//create an array of records (2 dimensional variable) called $arrayp.
-//populate it with the records retrieved by the SQL query previously executed.
-//Iterate through the array i.e while the end of the array has not been reached, run through it
-while ($arrayp = mysqli_fetch_array($exeSQL)) {
-    echo "<tr>";
-    echo "<td style='border: 0px'>";
-    echo "<a href=prodbuy.php?u_prod_id=" . $arrayp['prodId'] . "&view=yes>";
-    //display the small image whose name is contained in the array
-    echo "<img src=images/" .$arrayp['prodPicNameSmall']. " height=250 width=250>";
-    echo "</a>";
-    echo "</td>";
-    echo "<td style='border: 0px'>";
-    echo "<p><h5>" . $arrayp['prodName'] . "</h5>"; //display product name as contained in the array
-    echo "<p>" . $arrayp['prodDescripShort'];
-    echo "<p><b>&euro;" . $arrayp['prodPrice'] . "</b>";
-    echo "<p><a href=ratings.php?u_prod_id=" . $arrayp['prodId'] . ">Test Ratings</a></p>";
-    echo "</td>";
-    echo "</tr>";
-}
-echo "</table>";
+    $SQL = "select prodId, prodName, prodPicNameSmall, prodDescripShort, prodPrice from Product";
+    //run SQL query for connected DB or exit and display error message
+    $exeSQL = mysqli_query($conn, $SQL) or die(mysqli_error($conn));
+    echo "<table style='border: 0px'>";
+    //create an array of records (2 dimensional variable) called $arrayp.
+    //populate it with the records retrieved by the SQL query previously executed.
+    //Iterate through the array i.e while the end of the array has not been reached, run through it
+    while ($arrayp = mysqli_fetch_array($exeSQL)) {
+        echo "<tr>";
+        echo "<td style='border: 0px'>";
+        echo "<a href=prodbuy.php?u_prod_id=" . $arrayp['prodId'] . "&view=yes>";
+        //display the small image whose name is contained in the array
+        echo "<img src=images/" .$arrayp['prodPicNameSmall']. " height=250 width=250>";
+        echo "</a>";
+        echo "</td>";
+        echo "<td style='border: 0px'>";
+        echo "<p><h5>" . $arrayp['prodName'] . "</h5>"; //display product name as contained in the array
+        echo "<p>" . $arrayp['prodDescripShort'];
+        echo "<p><b>&euro;" . $arrayp['prodPrice'] . "</b>";
+        echo "<p><a href=ratings.php?u_prod_id=" . $arrayp['prodId'] . ">Test Ratings</a></p>";
+        echo "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
 }
 
 
